5a9a85376a06c5dc21cbebde83d96dd593ea4180
ongdv <ongdv94@gmail.com>
2022-04-23 01:24:17 +0900

    Update index.js

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import {  Route, Routes } from "react-router-dom";
-import { HomeLayout } from "../Components";
-import { About, Blog, Home } from "./pages";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { HomeLayout } from '../Components';
+import { About, Blog, Home } from './pages';
 
 const RootRouter = () => {
   return (
     <Routes>
-      <Route element={<HomeLayout/>}>
-        <Route index  element={<Home/>} />
-        <Route path="about" element={<About/>} />
-        <Route path="blog" element={<Blog/>} />
-        <Route path="blog/:blogId" element={<Blog/>} />
+      <Route path="/" element={<HomeLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:blogId" element={<Blog />} />
       </Route>
     </Routes>
   );
